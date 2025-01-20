f3e7c06e2860fda5b6f492756511eaaccfa3808a
DESKTOP-OO7EOJK\user1 <kh_user>
2025-01-20 10:32:51 +0900

    공통 컴포넌트 표, 게시글 리스트 디자인 수정

--- healthcare_react/healinglog/src/components/BoardList.jsx
+++ healthcare_react/healinglog/src/components/BoardList.jsx
@@ -5,26 +5,25 @@ const StyledTable = styled.table`
   width: ${(props) => props.width || '100%'};
   border-collapse: separate;
   border-spacing: 0;
-  margin: 20px 0;
   text-align: center;
   border: 1px solid #d0d7de;
   overflow: hidden;
 
   & th {
     background-color: ${(props) => props.thBgColor || '#BAD8B6'};
-    color: #24292f;
+    color: ${(props) => props.thTextColor || '#24292f'};
     font-weight: 600;
     padding: 12px 15px;
-    font-size: 14px;
+    font-size: ${(props) => props.thTextSize || '14px'};
     border-right: 1px solid #fafafa;
     border-bottom: 1px solid #fafafa;
   }
 
   & td {
-    background-color: #ffffff;
-    color: #393e44;
+    background-color: ${(props) => props.tdBgColor || '#ffffff'};
+    color: ${(props) => props.tdTextColor || '#393e44'};
     padding: 12px 15px;
-    font-size: 14px;
+    font-size: ${(props) => props.tdTextSize || '14px'};
     border-bottom: 1px solid #d0d7de;
     border-right: 1px solid #d0d7de;
   }
@@ -43,10 +42,20 @@ const StyledTable = styled.table`
   }
 `;
 
-const BoardList = ({ children }) => {
+const BoardList = ({ children, width, thBgColor, thTextColor, thTextSize, tdBgColor, tdTextColor, tdTextSize }) => {
   return (
     <>
-      <StyledTable>{children}</StyledTable>
+      <StyledTable
+        width={width}
+        thBgColor={thBgColor}
+        thTextColor={thTextColor}
+        thTextSize={thTextSize}
+        tdBgColor={tdBgColor}
+        tdTextColor={tdTextColor}
+        tdTextSize={tdTextSize}
+      >
+        {children}
+      </StyledTable>
     </>
   );
 };
