5f59f0c84d31af1a2fa8f7c9ce2a7874094106cc
kyj17 <yj9826@gmail.com>
2025-01-22 12:38:27 +0900

    feat: 음주 페이지 1236 수정

--- healthcare_react/healinglog/src/components/pages/LivingHealth/Alc/Alc.jsx
+++ healthcare_react/healinglog/src/components/pages/LivingHealth/Alc/Alc.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
 import Title from '../../../util/Title';
 import { setSelection } from '../../../../redux/selectSlice';
 import SelectedBar from '../../../util/SelectedBar';
+import styled from 'styled-components';
+import Calendar from '../../../util/Calendar';
+import Modal from '../../../util/Modal';
+
+const SubTitle = styled.div`
+  font-size: 35px;
+  font-weight: 600;
+  color: gray;
+  margin-top: 30px;
+  margin-bottom: 50px;
+  margin-left: 50px;
+`;
+
+const Highlight = styled.span`
+  border-bottom: 6px solid #ff7f50; /* 주황색 밑줄 */
+  padding-bottom: 5px;
+  color: black;
+`;
+
+const Box = styled.span``;
 
 const Alc = () => {
   return (
     <div>
       <Title>음주</Title>
-      <h1>음주 선택</h1>
+      <SubTitle>
+        <Highlight>캘린더</Highlight>&nbsp;&nbsp;리포트
+      </SubTitle>
 
+      <Box>
+        <Calendar></Calendar>
+      </Box>
       <SelectedBar label="alcohol" options={['소주', '맥주', '막걸리', '와인']} reduxAction={setSelection} index={0} />
       <SelectedBar label="alcohol" options={['주 1회', '주 2~3회', '매일']} reduxAction={setSelection} index={1} />
+
+      <Modal></Modal>
     </div>
   );
 };
