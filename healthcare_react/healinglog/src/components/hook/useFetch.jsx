069e7b6b29b89b631d5137742722983204a61631
DESKTOP-B81N8N2\sung <doeoddl1@gmail.com>
2025-01-22 11:33:19 +0900

    feat:Add calendar

---
+++ healthcare_react/healinglog/src/components/hook/useFetch.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+const useFetch = (url, options) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(url, options)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [url, options]);
+
+  return { data, setData }; // 데이터와 갱신 함수를 반환
+};
+
+export default useFetch;
--- healthcare_react/healinglog/src/components/pages/CardiovascularManagement/BloodPressure/BloodPressure.jsx
+++ healthcare_react/healinglog/src/components/pages/CardiovascularManagement/BloodPressure/BloodPressure.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Title from '../../../util/Title';
 import Calendar from '../../../util/Calendar';
+import Modal from '../../../util/Modal';
+import ModalInput from '../../../util/ModalInput';
 
 const BloodPressure = () => {
   return (
     <div>
       <Title>혈압</Title>
       <Calendar />
+
+      <ModalInput title="안녕ㅋㅋ"></ModalInput>
     </div>
   );
 };
