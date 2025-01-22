303e102564b9dfad6c2aa2ccc34854da6b11e320
abc <abc>
2025-01-22 11:41:34 +0900

    feat : Exercise 1차 작업

--- healthcare_react/healinglog/src/components/pages/LivingHealth/Exercise/Exercise.jsx
+++ healthcare_react/healinglog/src/components/pages/LivingHealth/Exercise/Exercise.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import Title from '../../../util/Title';
+import Navi from '../../../util/Navi';
+import styled from 'styled-components';
 
 const Exercise = () => {
   return (
     <>
-      <Title>운동</Title>
+      <Container>
+        <Title>운동</Title>
+        <NaviContainer>
+          <Navi target="exercise" tag={'유산소'}></Navi>
+          <Navi target="exercise2" tag={'무산소'}></Navi>
+          <Navi target="exlist" tag={'내역 관리'}></Navi>
+          <Navi target="report" tag={'리포트'}></Navi>
+        </NaviContainer>
+      </Container>
     </>
   );
 };
+
+const Container = styled.div``;
+
+const NaviContainer = styled.div`
+  display: grid;
+  position: relative;
+  width: 400px;
+  top: 20px;
+  left: 40px;
+  grid-template-columns: 3fr 3fr 4fr 3fr;
+`;
 
 export default Exercise;
