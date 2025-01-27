b7d38e351e5bc09ffd3dff547806c70b173fe76d
DESKTOP-OO7EOJK\user1 <kh_user>
2025-01-27 09:15:54 +0900

    feat : 에디터 컴포넌트 작성

--- healthcare_react/healinglog/src/components/pages/Board/BoardWrite.jsx
+++ healthcare_react/healinglog/src/components/pages/Board/BoardWrite.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Title from '../../util/Title';
 import BoardEditor from '../../util/BoardEditor';
+import styled from 'styled-components';
 
+const FormDiv = styled.div`
+  margin: 5px 50px;
+`;
 const BoardWrite = () => {
   return (
     <div>
       <Title>꿀팁게시판</Title>
-      <div>게시글 작성</div>
-      <label>제목</label>
-      <br />
-      <input></input>
-      <br />
-      <label>내용</label>
-      <br />
-      <BoardEditor />
+      <FormDiv>
+        <div>게시글 작성</div>
+        <label>제목</label>
+        <br />
+        <input></input>
+        <br />
+        <label>내용</label>
+        <br />
+        <BoardEditor />
+      </FormDiv>
     </div>
   );
 };
--- healthcare_react/healinglog/src/components/util/BoardEditor.jsx
+++ healthcare_react/healinglog/src/components/util/BoardEditor.jsx
@@ -1,105 +1,105 @@
-import React, { useState } from 'react';
-import { Editor, EditorState, RichUtils, convertToRaw, Entity, DefaultDraftBlockRenderMap } from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import React, { useState, useRef } from 'react';
+import { convertToRaw } from 'draft-js';
+import Editor, { createEditorStateWithText } from '@draft-js-plugins/editor';
+import createTextAlignmentPlugin from '@draft-js-plugins/text-alignment';
+import createLinkPlugin from '@draft-js-plugins/anchor';
+import '@draft-js-plugins/static-toolbar/lib/plugin.css';
+import '@draft-js-plugins/text-alignment/lib/plugin.css';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+} from '@draft-js-plugins/buttons';
+import createToolbarPlugin from '@draft-js-plugins/static-toolbar';
 import styled from 'styled-components';
-import { Map } from 'immutable';
 
-const ButtonDiv = styled.div`
-  border: 1px solid #ececec;
-  display: flex;
-  padding: 6px;
-  gap: 2px;
-`;
+const staticToolbarPlugin = createToolbarPlugin();
+const textAlignmentPlugin = createTextAlignmentPlugin();
+const linkPlugin = createLinkPlugin({
+  placeholder: 'http://…',
+});
+const { Toolbar } = staticToolbarPlugin;
+const plugins = [staticToolbarPlugin, textAlignmentPlugin, linkPlugin];
+const text = '';
 
-const IconButton = styled.button`
-  background-color: white;
-  border: none;
-  width: 28px;
-  height: 28px;
-  padding: 5px;
-`;
-
-const StyledImg = styled.img`
-  width: ${(props) => props.width || '100%'};
+const LayoutDiv = styled.div`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: auto 400px;
+  width: 100%;
   height: 100%;
 `;
 
-const EditorContainer = styled.div`
-  border: 1px solid #ddd;
-  min-height: 200px;
-  padding: 10px;
+const ContentDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 18px;
+  border-left: 1px solid #d8d8d8;
+  border-right: 1px solid #d8d8d8;
+  border-bottom: 1px solid #d8d8d8;
+  overflow: auto;
 
-  .leftAlign {
-    text-align: left;
-  }
-
-  .centerAlign {
-    text-align: center;
-  }
-
-  .rightAlign {
-    text-align: right;
+  .public-DraftEditorPlaceholder-root {
+    color: #808080;
   }
 `;
 
 const BoardEditor = () => {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const [editorState, setEditorState] = useState(() => createEditorStateWithText(text));
+  const [isFocused, setIsFocused] = useState(false);
 
-  const toggleInlineStyle = (style) => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, style));
+  const editorRef = useRef(null);
+
+  const focus = () => {
+    if (editorRef.current) {
+      editorRef.current.focus();
+      setIsFocused(true);
+    }
   };
 
-  const toggleBlockType = (blockType) => {
-    setEditorState(RichUtils.toggleBlockType(editorState, blockType));
-  };
-
-  // 에디터 콘텐츠를 JSON으로 저장
   const saveContent = () => {
     const rawContent = convertToRaw(editorState.getCurrentContent());
+    console.log(rawContent);
     console.log('Saved Content:', JSON.stringify(rawContent));
   };
 
   return (
-    <>
-      <div style={{ border: '1px solid #ccc', padding: '10px' }}>
-        <ButtonDiv>
-          <IconButton onClick={() => toggleInlineStyle('BOLD')}>
-            <StyledImg src="https://img.icons8.com/?size=100&id=78937&format=png&color=000000" />
-          </IconButton>
-          <IconButton onClick={() => toggleInlineStyle('ITALIC')}>
-            <StyledImg src="https://img.icons8.com/?size=100&id=99640&format=png&color=000000" />
-          </IconButton>
-          <IconButton onClick={() => toggleInlineStyle('UNDERLINE')}>
-            <StyledImg src="https://img.icons8.com/?size=100&id=99845&format=png&color=000000" />
-          </IconButton>
-
-          <IconButton>
-            <StyledImg src="https://img.icons8.com/?size=100&id=J3mppEmJhtqg&format=png&color=000000" />
-          </IconButton>
-          <IconButton>
-            <StyledImg src="https://img.icons8.com/?size=100&id=Mw0fxlGLXJlb&format=png&color=000000" />
-          </IconButton>
-          <IconButton>
-            <StyledImg src="https://img.icons8.com/?size=100&id=LcazPDNFek1d&format=png&color=000000" />
-          </IconButton>
-
-          <IconButton onClick={() => toggleBlockType('unordered-list-item')}>
-            <StyledImg src="https://img.icons8.com/?size=100&id=20440&format=png&color=000000" />
-          </IconButton>
-          <IconButton onClick={() => toggleBlockType('ordered-list-item')}>
-            <StyledImg src="https://img.icons8.com/?size=100&id=6501&format=png&color=000000" />
-          </IconButton>
-
-          {/* 저장 버튼 */}
-          {/* <button onClick={saveContent}>Save Content</button> */}
-        </ButtonDiv>
-
-        {/* 에디터 */}
-        <EditorContainer>
-          <Editor editorState={editorState} onChange={setEditorState} placeholder="내용을 입력해주세요." />
-        </EditorContainer>
+    <LayoutDiv className="editor" onClick={focus}>
+      <div>
+        <Toolbar>
+          {(externalProps) => (
+            <>
+              <ItalicButton {...externalProps} />
+              <BoldButton {...externalProps} />
+              <UnderlineButton {...externalProps} />
+              <textAlignmentPlugin.TextAlignment {...externalProps} />
+              <HeadlineOneButton {...externalProps} />
+              <HeadlineTwoButton {...externalProps} />
+              <HeadlineThreeButton {...externalProps} />
+              <UnorderedListButton {...externalProps} />
+              <OrderedListButton {...externalProps} />
+              <linkPlugin.LinkButton {...externalProps} />
+              <button onClick={saveContent}>Save Content</button>
+            </>
+          )}
+        </Toolbar>
       </div>
-    </>
+      <ContentDiv>
+        <Editor
+          editorState={editorState}
+          onChange={setEditorState}
+          plugins={plugins}
+          ref={editorRef}
+          placeholder={isFocused ? '' : '내용을 입력해주세요.'}
+        />
+      </ContentDiv>
+    </LayoutDiv>
   );
 };
 
