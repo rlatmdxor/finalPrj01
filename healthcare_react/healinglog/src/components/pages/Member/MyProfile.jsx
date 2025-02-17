7bcb8a2c5e18dffde28f663b20a78e4b23ff3c5c
abc <abc>
2025-02-17 20:55:02 +0900

    feat : 마이프로필

---
+++ healthcare_react/healinglog/src/components/pages/Member/MyProfile.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Btn from '../../util/Btn';
+import styled, { useTheme } from 'styled-components';
+import { setProfile } from '../../../redux/JoinSlice';
+
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const { profile } = useSelector((state) => state.join);
+  const fileInputRef = useRef(null);
+  const theme = useTheme();
+
+  const token = localStorage.getItem('token');
+
+  //프로필 변경
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (!selectedFile) return;
+
+    const formData = new FormData();
+    formData.append('profileImage', selectedFile);
+
+    fetch('http://127.0.0.1:80/api/member/profileChange', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    })
+      .then((resp) => resp.text())
+      .then((data) => {
+        dispatch(setProfile(data));
+        console.log(data);
+
+        alert('프로필 변경 완료!');
+      });
+  };
+
+  //프로필 삭제
+  const handleFileDelete = async () => {
+    if (window.confirm('이미지를 삭제하겠습니까?')) {
+      dispatch(setProfile('https://healinglog-kh.s3.ap-northeast-2.amazonaws.com/default_profile.jpg'));
+
+      fetch('http://127.0.0.1:80/api/member/profileDelete', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: {},
+      })
+        .then((resp) => resp.text())
+        .then((data) => {
+          dispatch(setProfile(data));
+          console.log(data);
+
+          alert('프로필 삭제 완료!');
+        });
+      fileInputRef.current.value = '';
+    }
+  };
+
+  const handleImageClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  return (
+    <>
+      <ProfileContainer>
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+
+        <img
+          src={profile}
+          // alt="프로필 미리보기"
+          onClick={handleImageClick}
+          style={{
+            width: '150px',
+            height: '150px',
+            borderRadius: '50%',
+            cursor: 'pointer',
+            objectFit: 'cover',
+            border: '2px solid #ccc',
+          }}
+        />
+      </ProfileContainer>
+      <BtnContainer>
+        <Btn str="삭제" f={handleFileDelete} c={theme.gray} mt={'0'} mb={'0'} ml={'0'} mr={'0'}></Btn>
+      </BtnContainer>
+      <div></div>
+    </>
+  );
+};
+
+export default MyProfile;
+
+const ProfileContainer = styled.div`
+  display: grid;
+`;
+const BtnContainer = styled.div`
+  display: grid;
+  justify-items: end;
+  align-items: end;
+`;
