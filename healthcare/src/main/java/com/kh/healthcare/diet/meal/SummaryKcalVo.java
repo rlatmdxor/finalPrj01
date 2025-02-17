f41d79a18e6accb78b8f28ff92f93621390112f6
DESKTOP-OO7EOJK\user1 <kh_user>
2025-02-17 20:31:45 +0900

    feat : 식단 조회 기능 작업

---
+++ healthcare/src/main/java/com/kh/healthcare/diet/meal/SummaryKcalVo.java
@@ -0,0 +1,13 @@
+package com.kh.healthcare.diet.meal;
+
+import lombok.Data;
+
+@Data
+public class SummaryKcalVo {
+
+    private String memberNo;
+    private String dietDay;
+    private String mealCode;
+    private String summaryKcal;
+
+}
