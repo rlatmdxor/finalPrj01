b786550c0cf7c7821dec0b972dcbe5920ea31085
DESKTOP-OO7EOJK\user1 <kh_user>
2025-02-25 20:10:32 +0900

    feat : 메인 페이지 작업

---
+++ healthcare_react/healinglog/src/components/services/homeService.js
@@ -0,0 +1,43 @@
+const getNoticeList = async (token) => {
+  const resp = await fetch(`http://127.0.0.1:80/api/main/notice`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!resp.ok) {
+    throw new Error(`HTTP ERROR !!! ${resp.status}`);
+  }
+  const noticeList = resp.json();
+  return noticeList;
+};
+
+const getBoardList = async (token) => {
+  const resp = await fetch(`http://127.0.0.1:80/api/main/board`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!resp.ok) {
+    throw new Error(`HTTP ERROR !!! ${resp.status}`);
+  }
+  const boardList = resp.json();
+  return boardList;
+};
+
+const getReviewList = async (token) => {
+  const resp = await fetch(`http://127.0.0.1:80/api/main/review`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!resp.ok) {
+    throw new Error(`HTTP ERROR !!! ${resp.status}`);
+  }
+  const boardList = resp.json();
+  return boardList;
+};
+
+export { getNoticeList, getBoardList, getReviewList };
