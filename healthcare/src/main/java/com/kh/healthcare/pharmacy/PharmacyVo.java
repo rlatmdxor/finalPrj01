ac89d40f22930c5828a9e8ee8d108f96e835594a
kyj17 <yj9826@gmail.com>
2025-02-25 17:50:30 +0900

    feat : 검색기능

---
+++ healthcare/src/main/java/com/kh/healthcare/pharmacy/PharmacyVo.java
@@ -0,0 +1,11 @@
+package com.kh.healthcare.pharmacy;
+
+import lombok.Data;
+
+@Data
+public class PharmacyVo {
+    private String name;
+    private String tellNum;
+    private String postNum;
+    private String address;
+}
