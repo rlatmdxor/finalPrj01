9950a59215deb7e17152613286e70a47b6b7b853
kyj17 <yj9826@gmail.com>
2025-03-04 17:38:05 +0900

    fix : 동적쿼리 수정/react fetch 함수 추가

--- healthcare/src/main/java/com/kh/healthcare/hospital/HospitalMapper.java
+++ healthcare/src/main/java/com/kh/healthcare/hospital/HospitalMapper.java
@@ -11,47 +11,48 @@ public interface HospitalMapper {
     @Select("""
         <script>
             SELECT NAME, TELL_NUM, POST_NUM, ADDRESS, HOSPITAL_TYPE
-            FROM (
-                SELECT H.NAME, H.TELL_NUM, H.POST_NUM, H.ADDRESS, H.HOSPITAL_TYPE,
-                       ROW_NUMBER() OVER (ORDER BY H.NAME ASC) AS RN
-                FROM HOSPITAL H
-                WHERE 1=1
-                
-                <!-- 시/구/동 검색 -->
-                <if test='city != null and city != ""'>
-                    AND H.CITY = #{city}
-                </if>
-                <if test='district != null and district != ""'>
-                    AND H.DISTRICT = #{district}
-                </if>
-                <if test='dong != null and dong != ""'>
-                    AND H.DONG = #{dong}
-                </if>
+              FROM (
+                  SELECT H.NAME, H.TELL_NUM, H.POST_NUM, H.ADDRESS, H.HOSPITAL_TYPE,
+                         ROW_NUMBER() OVER (ORDER BY H.NAME ASC) AS RN
+                  FROM HOSPITAL H
+                  WHERE 1=1
+                  <!-- 시/구/동 검색 (없어도 검색 가능하도록 수정) -->
+                  <if test="(city != null and city != '') or (district != null and district != '') or (dong != null and dong != '')">
+                      <if test="city != null and city != ''">
+                          AND H.CITY = #{city}
+                      </if>
+                      <if test="district != null and district != ''">
+                          AND H.DISTRICT = #{district}
+                      </if>
+                      <if test="dong != null and dong != ''">
+                          AND H.DONG = #{dong}
+                      </if>
+                  </if>
 
-                <!-- 병원과 검색 -->
-                <if test='hospitalType != null and hospitalType != ""'>
-                    AND H.HOSPITAL_TYPE = #{hospitalType}
-                </if>
+                  <!-- 병원과 검색 (시/구/동 없이도 검색 가능) -->
+                  <if test="hospitalType != null and hospitalType != ''">
+                      AND H.HOSPITAL_TYPE = #{hospitalType}
+                  </if>
 
-                <!-- 검색 조건 추가 -->
-                <if test='searchType != null and searchType != "" and keyword != null and keyword != ""'>
-                    <choose>
-                        <when test='searchType == "name"'>
-                            AND H.NAME LIKE '%' || #{keyword} || '%'
-                        </when>
-                        <when test='searchType == "address"'>
-                            AND H.ADDRESS LIKE '%' || #{keyword} || '%'
-                        </when>
-                        <when test='searchType == "tellNum"'>
-                            AND H.TELL_NUM LIKE '%' || #{keyword} || '%'
-                        </when>
-                        <when test='searchType == "postNum"'>
-                            AND H.POST_NUM LIKE '%' || #{keyword} || '%'
-                        </when>
-                    </choose>
-                </if>
-            )
-            WHERE RN BETWEEN #{offset} + 1 AND #{offset} + #{size}
+                  <!-- 검색 조건 추가 -->
+                  <if test="searchType != null and searchType != '' and keyword != null and keyword != ''">
+                      <choose>
+                          <when test="searchType == 'name'">
+                              AND H.NAME LIKE '%' || #{keyword} || '%'
+                          </when>
+                          <when test="searchType == 'address'">
+                              AND H.ADDRESS LIKE '%' || #{keyword} || '%'
+                          </when>
+                          <when test="searchType == 'tellNum'">
+                              AND H.TELL_NUM LIKE '%' || #{keyword} || '%'
+                          </when>
+                          <when test="searchType == 'postNum'">
+                              AND H.POST_NUM LIKE '%' || #{keyword} || '%'
+                          </when>
+                      </choose>
+                  </if>
+              )
+              WHERE RN BETWEEN #{offset} + 1 AND #{offset} + #{size}
         </script>
     """)
     List<HospitalVo> searchHospitals(
