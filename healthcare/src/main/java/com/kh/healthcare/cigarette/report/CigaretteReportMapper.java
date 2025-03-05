2fbd010de13d20e4ec296dd5b5ead2a06e03ad25
kyj17 <yj9826@gmail.com>
2025-03-05 12:32:11 +0900

    feat : 음주 흡연 토큰설정/ routes 수정

--- healthcare/src/main/java/com/kh/healthcare/cigarette/report/CigaretteReportMapper.java
+++ healthcare/src/main/java/com/kh/healthcare/cigarette/report/CigaretteReportMapper.java
@@ -6,47 +6,49 @@ import java.util.List;
 
 @Mapper
 public interface CigaretteReportMapper {
-
-    @Select("SELECT NO, " +
-            "MEMBER_NO, " +
-            "TAR, " +
-            "CIGARETTE, " +
-            "TO_CHAR(START_DATE, 'YYYY-MM-DD') AS startDate, " +
-            "TO_CHAR(END_DATE, 'YYYY-MM-DD') AS endDate " +
-            "FROM RECORD_CIGARETTE " +
-            "WHERE MEMBER_NO = #{memberNo} " +
-            "ORDER BY END_DATE DESC")
-    List<CigaretteReportVo> getCigaretteReport(Long memberNo);
+    @Select("""
+            SELECT NO
+            ,MEMBER_NO
+            ,TAR
+            ,CIGARETTE
+            ,TO_CHAR(START_DATE, 'YYYY-MM-DD') AS startDate
+            ,TO_CHAR(END_DATE, 'YYYY-MM-DD') AS endDate
+            FROM RECORD_CIGARETTE
+            WHERE MEMBER_NO = #{memberNo}
+            ORDER BY END_DATE DESC
+            """)
+    List<CigaretteReportVo> list(String memberNo);
 
     @Insert("""
             INSERT INTO RECORD_CIGARETTE (NO, MEMBER_NO, CIGARETTE, TAR, START_DATE, END_DATE)
             VALUES
             (SEQ_CIGARETTE.NEXTVAL
-            ,'1'
-            ,#{cigarette}
-            ,#{tar}
-            ,#{startDate}
-            ,#{endDate}
+            ,#{memberNo}
+            ,#{vo.cigarette}
+            ,#{vo.tar}
+            ,#{vo.startDate}
+            ,#{vo.endDate}
             )
             """)
-    void write(CigaretteReportVo vo);
+    void write(@Param("memberNo") String memberNo , @Param("vo") CigaretteReportVo vo);
+
 
     @Update("""
             UPDATE RECORD_CIGARETTE
             SET
-               CIGARETTE = #{cigarette},
-               TAR = #{tar},
-               START_DATE = #{startDate},
-               END_DATE = #{endDate}
-            WHERE NO = #{no}
+               CIGARETTE = #{vo.cigarette},
+               TAR = #{vo.tar},
+               START_DATE = #{vo.startDate},
+               END_DATE = #{vo.endDate}
+            WHERE NO = #{vo.no} AND MEMBER_NO = #{memberNo}
             """)
-    void update(CigaretteReportVo vo);
+    void update(@Param("memberNo") String memberNo , @Param("vo") CigaretteReportVo vo);
 
 
     @Delete("""
-            DELETE RECORD_CIGARETTE
-            WHERE NO = #{no}
+            DELETE FROM RECORD_CIGARETTE
+            WHERE NO = #{no} AND MEMBER_NO = #{memberNo}
             """)
-    void delete(CigaretteReportVo vo);
+    void delete(@Param("memberNo") String memberNo ,@Param("no") String no);
 }
 
