6a502a4bfdbcbcd36b6d40c5b49d413a386bb955
kyj17 <yj9826@gmail.com>
2025-03-13 16:04:42 +0900

    feat : 병원 별점기능 / 지도기능

--- healthcare/src/main/java/com/kh/healthcare/hospital/HospitalVo.java
+++ healthcare/src/main/java/com/kh/healthcare/hospital/HospitalVo.java
@@ -4,13 +4,16 @@ import lombok.Data;
 
 @Data
 public class HospitalVo {
+    private Long no;
     private String name;
     private String tellNum;
     private String postNum;
     private String address;
     private String hospitalType;
+    private double locationX;
+    private double locationY;
     private int totalCount;
 
 
-    private int rating;
+    private double rating;
 }
