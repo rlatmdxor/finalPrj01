a62e13babaffd469d3a75d8280c10fb0147a7bbc
DESKTOP-B81N8N2\sung <doeoddl1@gmail.com>
2025-03-17 10:22:10 +0900

    feat:fix bpbs

--- healthcare_react/healinglog/src/components/pages/CardiovascularManagement/BloodPressure/BloodPressure.jsx
+++ healthcare_react/healinglog/src/components/pages/CardiovascularManagement/BloodPressure/BloodPressure.jsx
@@ -118,7 +118,9 @@ const fillWeekData = (data, year, week) => {
     .map((date) => {
       const filteredData = data.filter((d) => d.day === date);
 
-      return filteredData.length > 0 ? filteredData : [{ day: date, systole: null, diastole: null }];
+      return filteredData.length > 0
+        ? filteredData.sort((a, b) => a.time.localeCompare(b.time)) // 시간순 정렬
+        : [{ day: date, systole: null, diastole: null, time: '00:00' }];
     })
     .flat();
 };
@@ -129,7 +131,9 @@ const fillMonthData = (data, year, month) => {
     .map((date) => {
       const filteredData = data.filter((d) => d.day === date);
 
-      return filteredData.length > 0 ? filteredData : [{ day: date, systole: null, diastole: null }];
+      return filteredData.length > 0
+        ? filteredData.sort((a, b) => a.time.localeCompare(b.time)) // 시간순 정렬
+        : [{ day: date, systole: null, diastole: null, time: '00:00' }];
     })
     .flat();
 };
